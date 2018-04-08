310e4f4fddef011c97da9d4ef586ef024ac5faab
Roy Cathey <modestmusashi@Roys-MacBook-Pro.local>
2018-04-08 12:11:02 -0500

    upload image test

--- bot.js
+++ bot.js
@@ -8,10 +8,34 @@ function random(max) {
 }
 //const newStatus = telePics[random(telePics.length)].src;
 
-function postTweet() {
+/*function postTweet() {
     T.post('statuses/update', { status: telePics[random(telePics.length)].src }, function(err, data, response) {
         console.log(data)
       });
+}*/
+
+function postTweet() {
+    const b64content = fs.readFileSync(telePics[random(telePics.length)].src, { encoding: 'base64' })
+ 
+    // first we must post the media to Twitter 
+    T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+      // now we can assign alt text to the media, for use by screen readers and 
+      // other text-based presentations and interpreters 
+      var mediaIdStr = data.media_id_string
+      var altText = "bleep bloop"
+      var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+     
+      T.post('media/metadata/create', meta_params, function (err, data, response) {
+        if (!err) {
+          // now we can reference the media and post a tweet (media will attach to the tweet) 
+          var params = { status: '', media_ids: [mediaIdStr] }
+     
+          T.post('statuses/update', params, function (err, data, response) {
+            console.log(data)
+          })
+        }
+      })
+    })   
 }
 
 postTweet()
